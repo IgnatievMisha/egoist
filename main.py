975ad0c9b0214c4d0cfa873cfa28bea68c71cd11
IgnatievMisha <mishaross1604@gmail.com>
2023-05-16 19:44:52 +0300

    past exception

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ try:
 except:
     print("no problems")
 print("any code...")
+'''
+def checker(var):
+    if type(var)!=str:
+        raise TypeError(f'We dont working with {type(var)}, we need for str')
+    else:
+        return var
+checker(1234)
+'''
+class BuildingError(Exception):
+    def __str__(self):
+        return "Something get wrong"
+def check_material(amount, limit):
+    if amount>limit:
+        return "over"
+    else:
+        raise BuildingError()
+print(check_material(10,354))
